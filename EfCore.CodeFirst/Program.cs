95c5531ae3bcf896729e171bbdc6155c413d373f
Cihat SOLAK <54249736+cihatsolak@users.noreply.github.com>
2022-12-23 21:42:57 +0300

    FromSqlInterpolated, FromSqlRaw

--- EfCore.CodeFirst/Program.cs
+++ EfCore.CodeFirst/Program.cs
@@ -1,23 +1,33 @@
-﻿Initializer.Build();
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+Initializer.Build();
 
 using (var _context = new AppDbContext())
 {
-    var result = _context.Students.Join(_context.Teachers, student => student.Name, teacher => teacher.Name, (student, teacher) =>
-    new
-    {
-        StudentName = student.Name,
-        TeacherName = teacher.Name,
-        StudentAge = student.Age
-    }).ToList();
 
-    var result2 = (from students in _context.Students
-                   join teacher in _context.Teachers on students.Name equals teacher.Name
-                   select new
-                   {
-                       StudentName = students.Name,
-                       TeacherName = teacher.Name,
-                       StudentAge = students.Age
-                   }).ToList();
+    var product = _context.Students.FromSqlRaw<Student>("select * from students where Age={0}", 2).First();
+
+    SqlParameter sqlParameter = new("age", 2);
+    var product2 = _context.Students.FromSqlInterpolated($"select * from students where Age={sqlParameter}").First();
+
+
+    //var result = _context.Students.Join(_context.Teachers, student => student.Name, teacher => teacher.Name, (student, teacher) =>
+    //new
+    //{
+    //    StudentName = student.Name,
+    //    TeacherName = teacher.Name,
+    //    StudentAge = student.Age
+    //}).ToList();
+
+    //var result2 = (from students in _context.Students
+    //               join teacher in _context.Teachers on students.Name equals teacher.Name
+    //               select new
+    //               {
+    //                   StudentName = students.Name,
+    //                   TeacherName = teacher.Name,
+    //                   StudentAge = students.Age
+    //               }).ToList();
 
 
     //var product = _context.Products.AsNoTracking().First();
