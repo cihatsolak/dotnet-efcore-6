60ed2fd33cb0f9f804c3788a1dacec2bbbaa2d81
Cihat Solak <cihatsolak@hotmail.com>
2023-01-15 17:20:55 +0300

    add initial migration for concurrency

---
+++ Concurrency.Web/Models/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Concurrency.Web.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasKey(p => p.Id);
+            modelBuilder.Entity<Product>().Property(p => p.Id).UseIdentityColumn(1, 1);
+            modelBuilder.Entity<Product>().Property(p => p.Price).HasPrecision(18, 2);
+
+            modelBuilder.Entity<Product>().HasCheckConstraint("StockCanBeAtLeastZero", "[Stock]>=0");
+
+            modelBuilder.Entity<Product>().Property(p => p.RowVersion).IsRowVersion();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
