7dca55f95a61fb04d802f7431900f99082b3ff6e
Cihat Solak <cihatsolak@hotmail.com>
2023-01-27 00:14:11 +0300

    Closed transaction between two related tables

---
+++ V2/EfCore.CodeFirst.V2/Controllers/TransactionsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace EfCore.CodeFirst.V2.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly FinanceDbContext _context;
+
+        public TransactionsController(FinanceDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Structure()
+        {
+            Post post = new()
+            {
+                Title = "",
+                Content = ""
+            };
+
+            _context.Posts.Add(post); // 1 - Post Ekleme İşlemi
+
+            Tag tag = _context.Tags.First();
+            tag.Name = $"[Updated] {tag.Name}"; // 2 - Tag Güncelleme İşlemi
+
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult TwoRelatedTables()
+        {
+            UserDetail userDetail = new()
+            {
+                PhoneNumber = "5000000000"
+            };
+
+            _context.UserDetails.Add(userDetail);
+
+            User user = new()
+            {
+                Name = "Cihat",
+                Age = 1,
+                UserDetail = userDetail
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
