af00a2b3c5faf487663f6843947efce4048689fc
Cihat Solak <cihatsolak@hotmail.com>
2023-01-28 19:00:43 +0300

    Add shoe entity

---
+++ V2/EfCore.CodeFirst.V2/Data/Entities/Shoe.cs
@@ -0,0 +1,11 @@
+﻿namespace EfCore.CodeFirst.V2.Data.Entities
+{
+    public class Shoe
+    {
+        public string Color { get; set; }
+        public int Size { get; set; }
+
+        //Row version tipinin kesinlikle byte[] olması gerekmektedir.
+        public byte[] RowVersion { get; set; }
+    }
+}
